e90513c8ef8c7479ea9228d7ce5ea6acb8fb5feb
Kolkenn <leonel.ponce96@gmail.com>
2025-04-22 22:10:14 -0400

    Maze Solver Project Start

---
+++ src/helpers.py
@@ -0,0 +1,36 @@
+class Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self,point1,point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self,canvas,fill_color):
+        canvas.create_line(self.point1.x,self.point1.y,self.point2.x,self.point2.y,fill=fill_color,width=2)
+
+class Cell():
+    def __init__(self,x1,x2,y1,y2,window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._top_left_point, self._top_right_point, self._bottom_left_point,self._bottom_right_point = Point(x1,y1), Point(x1,y2), Point(x2,y1), Point(x2,y2)
+        self._win = window
+
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(Line(self._top_left_point,self._bottom_left_point),'black')
+        if self.has_right_wall:
+            self._win.draw_line(Line(self._top_right_point,self._bottom_right_point),'black')
+        if self.has_top_wall:
+            self._win.draw_line(Line(self._top_left_point,self._top_right_point),'black')
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(self._bottom_left_point,self._bottom_right_point),'black')
---
+++ src/main.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3 
+
+import random
+
+from window import *
+from helpers import *
+
+def main():
+    resolution_width,resolution_length = 960,540
+    win = Window(resolution_width, resolution_length)
+
+    # Generate two random lines, that fit in the resolution of the window.
+    # num_of_lines = 2
+    # for i in range(num_of_lines):
+    #     pointa = Point(random.randint(0,resolution_width),random.randint(0,resolution_length))
+    #     pointb = Point(random.randint(0,resolution_width),random.randint(0,resolution_length))
+    
+    # Generate two random cells, that fit in the resolution of the window.
+    # num_of_cells = 2
+    # for i in range(num_of_cells):
+    #     x1 = random.randint(0,resolution_width)
+    #     y1 = random.randint(0,resolution_length)
+
+    #     x2 = random.randint(0,resolution_width)
+    #     y2 = random.randint(0,resolution_length)
+
+    #     print(f"{x1},{y1} | {x2},{y2}")
+    #     Cell(x1,x2,y1,y2,win).draw()
+    
+    cell1 = Cell(133,104,493,308,win)
+    cell1.has_bottom_wall = False
+    cell1.draw()
+    cell2 = Cell(157,194,500,392,win)
+    cell2.draw()
+
+
+    win.wait_for_close()
+
+main()
